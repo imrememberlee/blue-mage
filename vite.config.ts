e54b9b0750a37707c9e1a719bf930d19e53a6ad7
Ben <imrememberlee@gmail.com>
2022-11-20 15:21:26 +0800

    adjust files base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/blue-mage/",
+  // base: "/blue-mage/",
+  base: "/",
   plugins: [vue()],
   resolve: {
     alias: {
